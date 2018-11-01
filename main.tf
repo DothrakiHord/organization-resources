af2ec2159e4a55c6c54a43e116b6b2fc0e6cb82c
Ben Menasha <bmenasha@gcpidentityexample-1.com>
2018-11-01 09:57:39 -0400

    more folders/projects

--- main.tf
+++ main.tf
@@ -18,10 +18,12 @@
 
 provider "google" {
   credentials = "${file("/root/.config/gcloud/application_default_credentials.json")}"
+  # credentials = "${file("/home/bmenasha/Downloads/gcpidentityexample-1-cf-1-6c0eaee5ccd6.json")}"
 }
 
 provider "gsuite" {
   credentials = "/root/.config/gcloud/application_default_credentials.json"
+  # credentials = "/home/bmenasha/Downloads/gcpidentityexample-1-cf-1-6c0eaee5ccd6.json"
   impersonated_user_email = "${var.admin_email}"
   oauth_scopes = [
     "https://www.googleapis.com/auth/admin.directory.group",
@@ -52,7 +54,7 @@ module "dothraki-horses-dev-1" {
   org_id            = "${var.organization_id}"
   folder_id         = "${google_folder.dothraki-horses-dev.id}"
   billing_account   = "${var.billing_account}"
-  create_group      = "false"
+  create_group      = "true"
   group_name        = "dothraki-horses-dev-1-editors"
 }
 
@@ -68,7 +70,7 @@ module "dothraki-horses-prod-1" {
   org_id            = "${var.organization_id}"
   folder_id         = "${google_folder.dothraki-horses-prod.id}"
   billing_account   = "${var.billing_account}"
-  create_group      = "false"
+  create_group      = "true"
   group_name        = "dothraki-horses-prod-1-editors"
 }
 
@@ -79,7 +81,7 @@ module "dothraki-horses-prod-2" {
   org_id            = "${var.organization_id}"
   folder_id         = "${google_folder.dothraki-horses-prod.id}"
   billing_account   = "${var.billing_account}"
-  create_group      = "false"
+  create_group      = "true"
   group_name        = "dothraki-horses-prod-2-editors"
 }
 
@@ -110,7 +112,7 @@ module "qohor-valyrian-prod-1" {
   org_id            = "${var.organization_id}"
   folder_id         = "${google_folder.qohor-valyrian-prod.id}"
   billing_account   = "${var.billing_account}"
-  create_group      = "false"
+  create_group      = "true"
   group_name        = "qohor-valyrian-prod-1-editors"
 }
 
@@ -136,7 +138,7 @@ module "lhazar-sheep-dev-1"{
   org_id            = "${var.organization_id}"
   billing_account   = "${var.billing_account}"
   folder_id         = "${google_folder.lhazar-sheep-dev.id}"
-  create_group      = "false"
+  create_group      = "true"
   group_name        = "lhazar-sheep-dev-1-editors"
   app_engine        = {"location" = "us-central"}
 }
